79e2b340edf242b2f3fb0b08c6c8b8252cad4447
Likhit Ajeesh <likhitajeesh20@gmail.com>
2022-01-28 02:34:01 +0530

    feat: setup grid layout

--- src/containers/Overview/Overview.jsx
+++ src/containers/Overview/Overview.jsx
@@ -2,7 +2,21 @@ import React from "react";
 
 const Overview = () => (
   <div>
-    <h1 className="text-black">Overview</h1>
+    <div className="grid grid-cols-3 pt-40 pl-40">
+      <div className="text-white">
+        <h1 className="text-2xl">Team</h1>
+        <p className="text-4xl">Bina Aloo Pyaz</p>
+      </div>
+      <div className="text-white pl-20">
+        <h1 className="text-2xl">Score</h1>
+        <p className="text-4xl">459</p>
+      </div>
+      <div className="text-white">
+        <h1 className="text-2xl">Members</h1>
+        <p>Likhit Ajeesh</p>
+        <p>Jeet Kaushik</p>
+      </div>
+    </div>
   </div>
 );
 
