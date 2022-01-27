2b735a11c173beca5b3d889a4bd7ee570116917f
Likhit Ajeesh <likhitajeesh20@gmail.com>
2022-01-28 03:17:29 +0530

    feat: no submission page

--- src/containers/Overview/Overview.jsx
+++ src/containers/Overview/Overview.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 
+import "./Overview.css";
+
 const Overview = () => (
-  <div>
-    <div className="grid grid-cols-3 pt-40 pl-40">
-      <div className="text-white">
+  <div className="pl-52">
+    <div className="grid grid-cols-3 pt-40">
+      <div className="text-white space-y-3">
         <h1 className="text-2xl">Team</h1>
-        <p className="text-4xl">Bina Aloo Pyaz</p>
+        <p className="text-4xl text-prpl">Bina Aloo Pyaz</p>
       </div>
-      <div className="text-white pl-20">
+      <div className="text-white space-y-3 pl-28">
         <h1 className="text-2xl">Score</h1>
         <p className="text-4xl">459</p>
       </div>
       <div className="text-white">
-        <h1 className="text-2xl">Members</h1>
+        <h1 className="text-2xl pb-2">Members</h1>
         <p>Likhit Ajeesh</p>
         <p>Jeet Kaushik</p>
+      </div>
+    </div>
+
+    <div className="text-white pt-24">
+      <h1 className="text-3xl">My Submissions</h1>
+      <div className="flex flex-col items-center space-y-10 pr-40">
+        <h1 className="text-4xl text-grey pt-20">No Submissions</h1>
+        <div className="flex justify-center items-center cursor-pointer z-10 grad-bg rounded-3xl w-60 h-14">
+          <div className="text-white text-xl">Begin Solving</div>
+        </div>
       </div>
     </div>
   </div>
