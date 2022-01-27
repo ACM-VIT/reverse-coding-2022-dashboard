41d9795e230e797bd2445c67c721674c90b3486c
pranavvdesai <pranavdesai.psd@gmail.com>
2022-01-27 11:49:16 +0530

    feat: sidebar

---
+++ src/components/Questions/Questions.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/function-component-definition */
+import React from "react";
+
+const Questions = () => {
+  console.log("Questions");
+  return (
+    <div>
+      <h1 className="text-black">Questions</h1>
+    </div>
+  );
+};
+
+export default Questions;
