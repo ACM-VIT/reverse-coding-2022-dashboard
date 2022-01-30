40d4772aba846f27297790616f4da1faa693df22
pranavvdesai <pranavdesai.psd@gmail.com>
2022-01-30 15:32:45 +0530

    feat: init test components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import Questions from "./components/Questions/Questions";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
+import store from "./redux/store";
+import Overview from "./components/LeaderBoard/Overview";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className=" md:ml-64">
-          <Switch>
-            <>
-              <Sidebar />
-              <Route exact path="/questions" component={Questions} />
-            </>
-          </Switch>
-        </div>
+const App = () => (
+  <div>
+    <Provider store={store}>
+      <div className=" md:ml-64">
+        <Switch>
+          <>
+            <Redirect exact from="/" to="/overview" />
+            <Sidebar />
+            <Route exact path="/questions" component={Overview} />
+            <Route exact path="/overview" component={Questions} />
+          </>
+        </Switch>
       </div>
-    );
-  }
-}
+    </Provider>
+  </div>
+);
 export default App;
