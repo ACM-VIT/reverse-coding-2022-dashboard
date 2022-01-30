e47ab209a8cc92b4efae7a6b28e47cc192a75b92
Likhit Ajeesh <likhitajeesh20@gmail.com>
2022-01-30 17:07:56 +0530

    feat: fetch data through  axios

--- src/components/Submissions/Submissions.jsx
+++ src/components/Submissions/Submissions.jsx
@@ -1,28 +1,44 @@
 /* eslint-disable react/no-array-index-key */
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Accordion from "./Accordion/Accordion";
-import "./Submissions.css";
-import data from "./Data";
 
-const Submissions = () => (
-  <div
-    id="sub"
-    className="overflow-y-scroll h-80 mt-2 w-11/12 md:w-7/12 lg:w-7/12 xl:w-9/12"
-  >
-    <article>
-      <div className="tabscontainer">
-        {data.map((props) => (
-          <Accordion
-            key={props.id}
-            id={props.id}
-            title={props.question}
-            content={props.description}
-            score={props.score}
-          />
-        ))}
-      </div>
-    </article>
-  </div>
-);
+import "./Submissions.css";
+
+const Submissions = () => {
+  const [submissions, setSubmissions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://mocki.io/v1/969499df-f089-4638-8159-8cf4d822126b")
+      .then((res) => {
+        setSubmissions(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div
+      id="sub"
+      className="overflow-y-scroll h-80 mt-2 w-11/12 md:w-7/12 lg:w-7/12 xl:w-9/12"
+    >
+      <article>
+        <div className="tabscontainer">
+          {submissions.map((sub) => (
+            <Accordion
+              key={sub.id}
+              id={sub.id}
+              title={sub.question}
+              content={sub.description}
+              score={sub.score}
+            />
+          ))}
+        </div>
+      </article>
+    </div>
+  );
+};
 
 export default Submissions;
