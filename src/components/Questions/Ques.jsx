37e5fbf703c6688b32b1eacc5566a15f0c7a9d00
Ananya Grover <ananya.grover2017@gmail.com>
2022-02-01 04:17:38 +0530

    fix: grid design

--- src/components/Questions/Ques.jsx
+++ src/components/Questions/Ques.jsx
@@ -11,7 +11,7 @@ import "./Ques.css";
 
 const Questions = () => {
   const [active, setActive] = useState({
-    windowsImage: false,
+    windowsImage: true,
     linuxImage: false,
     macImage: false,
   });
@@ -19,7 +19,7 @@ const Questions = () => {
   return (
     <div className="ques mx-auto">
       <div className="flex flex-col gap-9 ">
-        <div className="px-8 py-8 bg-color ">
+        <div className="px-8 py-8 bg-color box-radius">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc diam sit
           arcu vitae. Nisi, metus, adipiscing sit rhoncus sed. Massa,
           scelerisque vel hac velLorem ipsum dolor sit amet, consectetur
@@ -27,7 +27,7 @@ const Questions = () => {
           rhoncus sed. Massa, scelerisque vel hac vel.
         </div>
         <div className="flex flex-row gap-9 sec-height">
-          <div className="px-5 py-5 bg-color  test-case">
+          <div className="px-5 py-5 bg-color relative test-case box-radius">
             01Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc diam
             sit arcu vitae. Nisi, metus, adipiscing sit rhoncus sed. Massa,
             scelerisque vel hac velLorem ipsum dolor sit amet, consectetur
@@ -36,15 +36,21 @@ const Questions = () => {
             sit amet, consectetur adipiscing elit. Nunc diam sit arcu vitae.
             Nisi, metus, adipiscing sit rhoncus sed. Massa, scelerisque vel hac
             vel.
-            <Link to="/">
-              <div className="upload-btn text-white mt-24 flex">
-                Upload
-                <img className="ml-2 h-6" src={upload} alt="upload" />
+            <div className="flex absolute bottom-0 mb-4">
+              <Link to="/">
+                <div className="upload-btn text-white flex">
+                  Upload
+                  <img className="ml-2 h-6" src={upload} alt="upload" />
+                </div>
+              </Link>
+              <div className=" ml-10 text-white font-700 text-xl">
+                Points: <br />
+                25/100{" "}
               </div>
-            </Link>
+            </div>
           </div>
           <div className="flex flex-col gap-6 ">
-            <div className="bg-color pl-6 pt-6 pr-16 exec">
+            <div className="bg-color pl-6 pt-6 pr-16 exec box-radius">
               To solve the question on your PC, download the executable file
               accordinng to your OS.
               <div
@@ -72,8 +78,7 @@ const Questions = () => {
                   <img
                     src={windows}
                     alt="windows"
-                    className="icon"
-                    height="21"
+                    className="icon h-5"
                     style={{
                       cursor: "pointer",
                       marginLeft: "24px",
@@ -93,8 +98,7 @@ const Questions = () => {
                   <img
                     src={linux}
                     alt="linux"
-                    className="icon"
-                    height="21"
+                    className="icon h-5"
                     style={{
                       cursor: "pointer",
                       opacity: active.linuxImage ? "1" : "0.5",
@@ -113,8 +117,7 @@ const Questions = () => {
                   <img
                     src={mac}
                     alt="mac"
-                    className="icon"
-                    height="21"
+                    className="icon h-5"
                     style={{
                       cursor: "pointer",
                       opacity: active.macImage ? "1" : "0.5",
@@ -140,12 +143,12 @@ const Questions = () => {
                     // opacity: active. ? "1" : "0.5",
                   }}
                 >
-                  <img src={download} alt="download" height="21" />
+                  <img className="h-5" src={download} alt="download" />
                 </span>
               </div>
             </div>
             <div className="text-center text-2xl">OR</div>
-            <div className="bg-color exec pl-6 pt-6">
+            <div className="bg-color exec pl-6 pt-6 box-radius">
               Try with your own custom I/O online
               <Link to="/">
                 <div className="launch-btn text-white mt-16 mx-24 flex">
--- src/components/Questions/ide.jsx
+++ src/components/Questions/ide.jsx
@@ -12,147 +12,157 @@ import "./ide.css";
 
 const Questions = () => {
   const [active, setActive] = useState({
-    windowsImage: false,
+    windowsImage: true,
     linuxImage: false,
     macImage: false,
   });
 
   return (
     <div className="ide mx-auto">
-      <div className="flex flex-row gap-9 ">
-        <div className="px-5 py-5 bg-color  test-case">
-          01Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc diam
-          sit arcu vitae. Nisi, metus, adipiscing sit rhoncus sed. Massa,
+      <div className="flex flex-col gap-9 ">
+        <div className="px-8 py-8 bg-color ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc diam sit
+          arcu vitae. Nisi, metus, adipiscing sit rhoncus sed. Massa,
           scelerisque vel hac velLorem ipsum dolor sit amet, consectetur
           adipiscing elit. Nunc diam sit arcu vitae. Nisi, metus, adipiscing sit
-          rhoncus sed. Massa, scelerisque vel hac vel. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Nunc diam sit arcu vitae. Nisi,
-          metus, adipiscing sit rhoncus sed. Massa, scelerisque vel hac vel.
-          <Link to="/">
-            <div className="upload-btn text-white mt-72 flex">
-              Upload
-              <img className="ml-2 h-6" src={upload} alt="upload" />
-            </div>
-          </Link>
+          rhoncus sed. Massa, scelerisque vel hac vel.
+          <div
+            className="download-container mt-2 exec"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              overflow: "hidden",
+              height: "40px",
+              width: "190px",
+              marginRight: "1.2rem",
+            }}
+          >
+            <span
+              onClick={() => {
+                setActive({
+                  windowsImage: true,
+                  linuxImage: false,
+                  macImage: false,
+                });
+              }}
+            >
+              <img
+                src={windows}
+                alt="windows"
+                className="icon h-5"
+                style={{
+                  cursor: "pointer",
+                  marginLeft: "24px",
+                  opacity: active.windowsImage ? "1" : "0.5",
+                }}
+              />
+            </span>
+            <span
+              onClick={() => {
+                setActive({
+                  windowsImage: false,
+                  linuxImage: true,
+                  macImage: false,
+                });
+              }}
+            >
+              <img
+                src={linux}
+                alt="linux"
+                className="icon h-5"
+                style={{
+                  cursor: "pointer",
+                  opacity: active.linuxImage ? "1" : "0.5",
+                }}
+              />
+            </span>
+            <span
+              onClick={() => {
+                setActive({
+                  windowsImage: false,
+                  linuxImage: false,
+                  macImage: true,
+                });
+              }}
+            >
+              <img
+                src={mac}
+                alt="mac"
+                className="icon h-5"
+                h
+                style={{
+                  cursor: "pointer",
+                  opacity: active.macImage ? "1" : "0.5",
+                }}
+              />
+            </span>
+            <span
+              onClick={() => {
+                setActive({
+                  windowsImage: true,
+                  linuxImage: false,
+                  macImage: false,
+                });
+              }}
+              className="icon download"
+              style={{
+                marginRight: "0",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "2.75rem",
+                cursor: "pointer",
+                // opacity: active. ? "1" : "0.5",
+              }}
+            >
+              <img className="h-5" src={download} alt="download" />
+            </span>
+          </div>
         </div>
-        <div className="flex flex-col gap-9 ">
-          <div className="px-8 py-8 bg-color ques">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc diam
+        <div className="flex flex-row gap-9 ">
+          <div className="px-5 py-5 bg-color relative test-case">
+            01Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc diam
             sit arcu vitae. Nisi, metus, adipiscing sit rhoncus sed. Massa,
             scelerisque vel hac velLorem ipsum dolor sit amet, consectetur
             adipiscing elit. Nunc diam sit arcu vitae. Nisi, metus, adipiscing
-            sit rhoncus sed. Massa, scelerisque vel hac vel.
-          </div>
-          <div className="flex flex-row gap-7">
-            <div>
-              <div className="bg-color pl-6 pt-6  input">Input</div>
-              <div className="flex ">
-                <div
-                  className="download-container mt-8"
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    overflow: "hidden",
-                    height: "40px",
-                    width: "190px",
-                    marginRight: "1.2rem",
-                  }}
-                >
-                  <span
-                    onClick={() => {
-                      setActive({
-                        windowsImage: true,
-                        linuxImage: false,
-                        macImage: false,
-                      });
-                    }}
-                  >
-                    <img
-                      src={windows}
-                      alt="windows"
-                      className="icon"
-                      height="21"
-                      style={{
-                        cursor: "pointer",
-                        marginLeft: "24px",
-                        opacity: active.windowsImage ? "1" : "0.5",
-                      }}
-                    />
-                  </span>
-                  <span
-                    onClick={() => {
-                      setActive({
-                        windowsImage: false,
-                        linuxImage: true,
-                        macImage: false,
-                      });
-                    }}
-                  >
-                    <img
-                      src={linux}
-                      alt="linux"
-                      className="icon"
-                      height="21"
-                      style={{
-                        cursor: "pointer",
-                        opacity: active.linuxImage ? "1" : "0.5",
-                      }}
-                    />
-                  </span>
-                  <span
-                    onClick={() => {
-                      setActive({
-                        windowsImage: false,
-                        linuxImage: false,
-                        macImage: true,
-                      });
-                    }}
-                  >
-                    <img
-                      src={mac}
-                      alt="mac"
-                      className="icon"
-                      height="21"
-                      style={{
-                        cursor: "pointer",
-                        opacity: active.macImage ? "1" : "0.5",
-                      }}
-                    />
-                  </span>
-                  <span
-                    onClick={() => {
-                      setActive({
-                        windowsImage: true,
-                        linuxImage: false,
-                        macImage: false,
-                      });
-                    }}
-                    className="icon download"
-                    style={{
-                      marginRight: "0",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      width: "2.75rem",
-                      cursor: "pointer",
-                      // opacity: active. ? "1" : "0.5",
-                    }}
-                  >
-                    <img src={download} alt="download" height="21" />
-                  </span>
+            sit rhoncus sed. Massa, scelerisque vel hac vel. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Nunc diam sit arcu vitae.
+            Nisi, metus, adipiscing sit rhoncus sed. Massa, scelerisque vel hac
+            vel.
+            <div className="flex absolute bottom-0 mb-4">
+              <Link to="/">
+                <div className="upload-btn text-white flex">
+                  Upload
+                  <img className="ml-2 h-6" src={upload} alt="upload" />
                 </div>
-                <Link to="/">
-                  <div className="run-btn text-white mt-8 flex">
-                    Run
-                    <img className="ml-2 " src={run} alt="run" />
-                  </div>
-                </Link>
+              </Link>
+              <div className=" ml-10 text-white font-700 text-xl">
+                Points: <br />
+                25/100{" "}
               </div>
             </div>
+          </div>
+          <div className="flex flex-col gap-9 ">
+            <div className="flex flex-row gap-7">
+              <div>
+                <div className="bg-color pl-6 pt-6  input relative">
+                  Input
+                  <br />
+                  <textarea className="text-area" />
+                  <div className="flex ">
+                    <Link to="/">
+                      <div className="run-btn absolute bottom-0 mb-4 text-white  flex">
+                        <img src={run} alt="run" />
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
 
-            <div className="bg-color pl-6 pt-6 output">Output</div>
+              <div className="bg-color pl-6 pt-6 output">Output</div>
+            </div>
           </div>
         </div>
       </div>
