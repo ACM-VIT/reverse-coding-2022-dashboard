53da23eefd134f791ae608ce47b596a51b82063e
pranavvdesai <pranavdesai.psd@gmail.com>
2022-02-03 01:05:03 +0530

    fix: state leaderboard

--- src/containers/LeaderboardContainer/Leaderboard.jsx
+++ src/containers/LeaderboardContainer/Leaderboard.jsx
@@ -19,7 +19,7 @@ function Leaderboard() {
   const [maxpageNumberLimit, setMaxPageNumberLimit] = useState(8);
   const [minpageNumberLimit, setMinPageNumberLimit] = useState(0);
 
-  const getData = useSelector((state) => state.postTeam.people);
+  const getData = useSelector((state) => state.getAll.teams.participants);
   console.log(getData);
 
   const pages = [];
