da308eea66cce7f842e2e48c7dc435d2a1c071c3
ANISH0309 <mittalanish789@gmail.com>
2022-02-03 16:29:22 +0530

    style: fix styling of 404 page
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -6,30 +6,33 @@ import { Link } from "react-router-dom";
 import "./404.css";
 
 // Assets
-// import NotFound from "../../assets/images/404.svg";
+import Logo from "../../assets/images/logo.svg";
 
 const NotFound404 = () => (
   <div className="bg-image">
     <div className="absolute xxs:pl-12 xs:pl-16 sm:pl-20 pl-24 pt-8">
-      {/**  TODO: what path should be put here? */}
-      <Link to="/">{/* <img src={NotFound} alt="Logo" /> */}</Link>
+      <Link to="/">
+        <img src={Logo} alt="Logo" />
+      </Link>
     </div>
     <div className="flex h-screen justify-center items-center overflow-y-hidden">
       <div className="container mx-auto ">
         <div className="flex flex-col justify-center items-center mx-2 xs:text-center sm:text-center md:text-center lg:text-center xl:text-center xf:text-center xxl:text-center">
-          <div className="text-5xl text-white font-700 pb-8">
+          <div className="2xl:text-6xl 3xl:text-7xl text-5xl text-white font-700 pb-8">
             404 - Looks like you're lost.
           </div>
-          <div className="text-xl mt-2 text-white font-400 xxs:px-0 xs:px-0  px-20">
+          <div className="2xl:text-2xl 3xl:text-3xl text-xl mt-2 text-white font-400 xxs:px-0 xs:px-0  px-20">
             <p>Looks like this page doesn't exist.</p>
             <p>But don't let that stop you!</p>
           </div>
-          <div className="text-xl mt-2 text-white font-400 pt-4">
+          <div className="2xl:text-2xl 3xl:text-3xl text-xl mt-2 text-white font-400 pt-4">
             Head back to the home page right away!
           </div>
-          <Link to="/register">
-            <div className="mt-12 font-semibold py-2 px-4 cursor-pointer brdr">
-              <span className="grad">Go Back</span>
+          <Link to="/">
+            <div className="mt-12 grad-bg text-white font-400 py-2 px-4 w-28 2xl:w-32 3xl:w-36 cursor-pointer rounded-xl">
+              <span className="text-normal 2xl:text-md 3xl:text-lg">
+                Go Back
+              </span>
             </div>
           </Link>
         </div>
