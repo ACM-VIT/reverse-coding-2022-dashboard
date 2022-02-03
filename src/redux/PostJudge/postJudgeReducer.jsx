caaa1bb49e70e56e61504371e8db246b2ad2fdac
pranavvdesai <pranavdesai.psd@gmail.com>
2022-02-03 16:44:10 +0530

    fix: judge response object

--- src/redux/PostJudge/postJudgeReducer.jsx
+++ src/redux/PostJudge/postJudgeReducer.jsx
@@ -1,19 +1,65 @@
 /* eslint-disable default-param-last */
-import { POST_FILE } from "./postJudgeTypes";
-
+import {
+  POST_FILE,
+  CASE_ONE,
+  CASE_TWO,
+  CASE_THREE,
+  CASE_FOUR,
+  CASE_FIVE,
+  JUDGE_MAIN,
+} from "./postJudgeTypes";
+import { CODE_STATES } from "./states";
 const initialState = {
   postjudge: [],
+  judgestatecol: "",
+  judgestatetext: "",
+  judgeMain: [],
 };
 
-const postTeamReducer = (state = initialState, action) => {
+const postJudgeReducer = (state = initialState, action) => {
   switch (action.type) {
     case POST_FILE:
       return {
         ...state,
         postjudge: action.payload,
       };
+    case CASE_ONE:
+      return {
+        ...state,
+        judgestatecol: CODE_STATES[action.payload].color,
+        judgestatetext: CODE_STATES[action.payload].text,
+      };
+    case CASE_TWO:
+      return {
+        ...state,
+        judgestatecol: CODE_STATES[action.payload].color,
+        judgestatetext: CODE_STATES[action.payload].text,
+      };
+    case CASE_THREE:
+      return {
+        ...state,
+        judgestatecol: CODE_STATES[action.payload].color,
+        judgestatetext: CODE_STATES[action.payload].text,
+      };
+    case CASE_FOUR:
+      return {
+        ...state,
+        judgestatecol: CODE_STATES[action.payload].color,
+        judgestatetext: CODE_STATES[action.payload].text,
+      };
+    case CASE_FIVE:
+      return {
+        ...state,
+        judgestatecol: CODE_STATES[action.payload].color,
+        judgestatetext: CODE_STATES[action.payload].text,
+      };
+    case JUDGE_MAIN:
+      return {
+        ...state,
+        judgeMain: action.payload,
+      };
     default:
       return state;
   }
 };
-export default postTeamReducer;
+export default postJudgeReducer;
---
+++ src/redux/PostJudge/states.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+export const CODE_STATES = [
+  {
+    text: "Processing",
+    color: "blue",
+  },
+  {
+    text: "In Queue, there are many competitive souls working hard like you",
+    color: "blue",
+  },
+  {
+    text: "Code Executing...",
+    color: "blue",
+  },
+  {
+    text: "Dope ! Solution Accepted",
+    color: "green",
+  },
+  { text: "Oh ! Wrong Answer", color: "red" },
+  { text: "Time Limit Exceeded, try improving the code", color: "yellow" },
+  { text: "Oh ! Your code couldn't be compiled", color: "white" },
+  { text: "Runtime Error !", type: "error" },
+  { text: "Runtime Error !", type: "error" },
+  { text: "Runtime Error !", type: "error" },
+  { text: "Runtime Error !", type: "error" },
+  { text: "Runtime Error !", type: "error" },
+  { text: "Runtime Error !", type: "error" },
+  { text: "Runtime Error !", type: "error" },
+  { text: "Runtime Error !", type: "error" },
+];
