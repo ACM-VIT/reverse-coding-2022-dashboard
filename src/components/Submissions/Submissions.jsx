8089b873395cc2685662315c56285b3126b1370a
Likhit Ajeesh <likhitajeesh20@gmail.com>
2022-02-03 06:01:11 +0530

    fetch(submission): data from api

--- src/components/Submissions/Submissions.jsx
+++ src/components/Submissions/Submissions.jsx
@@ -1,28 +1,21 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
 import Accordion from "./Accordion/Accordion";
 
 import "./Submissions.css";
 
 const Submissions = () => {
-  const [submissions, setSubmissions] = useState([]);
+  // const [submissions, setSubmissions] = useState([]);
+  const getProblems = useSelector((state) => state.getAll.problems);
 
-  useEffect(() => {
-    axios
-      .get("https://mocki.io/v1/14e66659-cc6d-45d5-a1a0-2ca05f1ebea2")
-      .then((res) => {
-        setSubmissions(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  const submissions = getProblems;
 
   return (
     <div
       id="sub"
-      className="overflow-y-scroll h-80 2xl:h-100 mt-4 2xl:mt-8 w-9/12 2xl:w-10/12"
+      className="overflow-y-scroll min-h-fit max-h-80 2xl:max-h-100 mt-4 2xl:mt-8 w-9/12 2xl:w-10/12"
     >
       <article>
         <div className="mr-5">
@@ -30,9 +23,9 @@ const Submissions = () => {
             <Accordion
               key={sub.id}
               id={sub.id}
-              title={sub.question}
-              content={sub.description}
-              score={sub.score}
+              question={sub.name}
+              desc={sub.instructionsText}
+              score={sub.maxPoints}
             />
           ))}
         </div>
