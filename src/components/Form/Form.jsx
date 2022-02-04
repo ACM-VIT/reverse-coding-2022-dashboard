6bed8f54b6cff4c3163a4fcb9615cf9fd145ace1
ANISH0309 <mittalanish789@gmail.com>
2022-02-04 17:06:00 +0530

    fix: url
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -28,9 +28,10 @@ const Form = () => {
       authorization: `Bearer ${token}`,
     };
     axios
-      .get(`${process.env.BASEURL}/participants`, { headers })
+      .get(`${process.env.REACT_APP_BASEURL}/participants`, { headers })
       .then((res) => {
         const firstName = res.data.name.split(" ")[0];
+        console.log(firstName);
         setDisplayName(firstName);
       })
       .catch((err) => {
@@ -85,12 +86,13 @@ const Form = () => {
 
     axios
       .post(
-        `${process.env.BASEURL}/participants/update`,
+        `${process.env.REACT_APP_BASEURL}/participants/update`,
         JSON.stringify(data),
         { headers }
       )
       .then(() => {
         window.location.href = "/overview";
+        sessionStorage.setItem("FF", true);
       })
       .catch((err) => {
         console.log(err);
